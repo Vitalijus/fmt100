f95c292fae634b6c549d9bf5e74a3f083091ad66
vitaly <vitalij.desuk@gmail.com>
2022-05-31 14:13:19 +0300

    add GQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  # GQL playground http://localhost:3000/graphiql
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
 
+  # GQL endpoint
+  post "/graphql", to: "graphql#execute"
+
+  # Resources
   resources :trackers
   resources :orders, only: [:new, :create]
   resources :vehicles
 
+  # Custom pages
   get "my-vehicles", to: "vehicles#my_vehicles"
   get "my-orders", to: "orders#my_orders"
+  get "contacts", to: "pages#contacts"
 
+  # Devise
   devise_for :users
 
   # Sidekiq
@@ -15,9 +26,7 @@ Rails.application.routes.draw do
   require "sidekiq_unique_jobs/web"
   mount Sidekiq::Web, at: "/sidekiq"
 
-  # pages
-  get "contacts", to: "pages#contacts"
-
+  # Home page
   root to: "vehicles#index"
 
   # Redirect all unknown routes to root_url / keep at the bottom of this file
