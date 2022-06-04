1f973f5e6a60f974e491f311dbe9e45faf6244c2
vitaly <vitalij.desuk@gmail.com>
2022-06-04 14:01:31 +0300

    add distance price

--- app/graphql/mutations/odometers/create_update_odometer.rb
+++ app/graphql/mutations/odometers/create_update_odometer.rb
@@ -7,7 +7,7 @@ module Mutations
       argument :vehicle_id, ID, required: true
       argument :destination_address, String, required: false
       argument :origin_address, String, required: false
-      argument :distance, Int, required: false
+      argument :distance, Int, required: true
       argument :duration, Int, required: false
       argument :city, String, required: false
       argument :within_radius, Boolean, required: false
@@ -16,7 +16,14 @@ module Mutations
       type Types::OdometerType
 
       def resolve(args)
+        args[:distance_price] = distance_price(args[:vehicle_id], args[:distance]) if args[:distance_price].nil?
         Odometer.create!(args)
+      end
+
+      def distance_price(vehicle_id, distance)
+        vehicle = Vehicle.find(vehicle_id)
+
+        (distance.to_d / 1000.to_d) * vehicle.km_price
       end
     end
   end
